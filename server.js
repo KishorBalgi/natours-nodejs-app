adfa693cb24ebe2a6aac3618d6c87b0e7fe6047c
Kishor Balgi <kishorbalgi@gmail.com>
2022-02-19 21:03:28 +0530

    Prepared for deployment

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ mongoose
   });
 
 // Server:
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
